be1e30b943a6898991c55b1047ac13164305fab9
Oleksandr Kachanov <okacha@softserveinc.com>
2017-01-27 17:28:52 +0200

    Started creating transactions controller

---
+++ finapp/UI/TransactionsTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionsTableViewCell.swift
+//  finapp
+//
+//  Created by Oleksandr Kachanov on 1/27/17.
+//  Copyright © 2017 Oleksandr Kachanov. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsTableViewCell: UITableViewCell {
+
+    @IBOutlet var comment: UILabel!
+    @IBOutlet var sum: UILabel!
+    @IBOutlet var category: UILabel!
+    @IBOutlet var categoryImage: UIImageView!
+    @IBOutlet var date: UILabel!
+    
+    
+}
---
+++ finapp/UI/TransactionsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionsViewController.swift
+//  finapp
+//
+//  Created by Oleksandr Kachanov on 1/27/17.
+//  Copyright © 2017 Oleksandr Kachanov. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsViewController: UIViewController {
+
+
+    @IBOutlet var tableView: UITableView!
+    var account: FinAccount!
+    
+    
+    
+
+}
