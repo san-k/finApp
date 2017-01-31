fcdc2a634a364f3fc30b4057b37bcdb816768d12
Admin <admin@mac-admin.local>
2017-02-01 00:59:18 +0200

    Continue with new account UI

---
+++ finapp/UI/NewAccountViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewAccountViewController.swift
+//  finapp
+//
+//  Created by Admin on 01.02.17.
+//  Copyright © 2017 Oleksandr Kachanov. All rights reserved.
+//
+
+import UIKit
+
+class NewAccountViewController: UIViewController {
+
+
+}
