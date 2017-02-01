feea2a37c807b5c15361bb820a2624e620195a3b
Oleksandr Kachanov <okacha@softserveinc.com>
2017-02-01 21:06:02 +0200

    Continue with new account page

--- finapp/UI/NewAccountViewController.swift
+++ finapp/UI/NewAccountViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class NewAccountViewController: UIViewController {
+class NewAccountViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var nameText: UITextField!
     @IBOutlet var startSumText: UITextField!
     @IBOutlet var categoryTable: UITableView!
+    @IBOutlet var tableHeightConstraint: NSLayoutConstraint!
     @IBOutlet var commentTextView: UITextView!
 
+    var categories: [FinTransactionCategory]?
     
     
     // PRIVATE PROPERTIES
@@ -26,11 +28,25 @@ class NewAccountViewController: UIViewController {
     @IBAction func categoryTapped(_ sender: UIButton) {
     }
     
+    override func viewDidLoad() {
+        categoryTable.register(UINib(nibName: "TransactionCategoryTableViewCell", bundle: nil), forCellReuseIdentifier: TransactionCategoryTableViewCell.staticIdentifier)
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         
     }
     
+    //MARK: - table view data source
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories?.count ?? 0
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TransactionCategoryTableViewCell.staticIdentifier, for: indexPath) as! TransactionCategoryTableViewCell
+        return cell
+    }
 }
---
+++ finapp/UI/TransactionCategoryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TransactionCategoryTableViewCell.swift
+//  finapp
+//
+//  Created by Oleksandr Kachanov on 2/1/17.
+//  Copyright © 2017 Oleksandr Kachanov. All rights reserved.
+//
+
+import UIKit
+
+class TransactionCategoryTableViewCell: UITableViewCell {
+
+    static let staticIdentifier = "TransactionCategoryTableViewCellIdentifier"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
