a168b7ecb3c0a18997d53e951e3cf6fe6de6185a
Oleksandr Kachanov <okacha@softserveinc.com>
2017-02-01 11:06:43 +0200

    new account page

--- finapp/UI/NewAccountViewController.swift
+++ finapp/UI/NewAccountViewController.swift
@@ -10,5 +10,27 @@ import UIKit
 
 class NewAccountViewController: UIViewController {
 
+    @IBOutlet var nameText: UITextField!
+    @IBOutlet var startSumText: UITextField!
+    @IBOutlet var categoryTable: UITableView!
+    @IBOutlet var commentTextView: UITextView!
 
+    
+    
+    // PRIVATE PROPERTIES
+    private var account: FinAccount?
+    
+    
+    
+    
+    @IBAction func categoryTapped(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    
+    
 }
