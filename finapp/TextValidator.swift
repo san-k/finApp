87315fbb2cc7702d4d79b7ab13675c8608b93f24
Admin <admin@mac-admin.local>
2017-02-06 01:13:57 +0200

    Added IBInspectable properties to Tunable Text Field Started with Validator class

---
+++ finapp/TextValidator.swift
@@ -0,0 +1,31 @@
+//
+//  TextValidator.swift
+//  finapp
+//
+//  Created by Admin on 05.02.17.
+//  Copyright © 2017 Oleksandr Kachanov. All rights reserved.
+//
+
+import Foundation
+
+
+class TextValidator {
+    
+    private var textFieldsSet: Set<String>
+    
+    init(with set: Set<String>) {
+        textFieldsSet = set
+    }
+    
+    func validate(field: TunableTextField) -> Bool {
+        var matched = false
+        /* match regexp...*/
+        
+//        if matched {
+//            textFieldsSet.remove(<#T##member: Hashable##Hashable#>)
+//        }
+        
+        return matched
+    }
+    
+}
--- finapp/TunableTextField.swift
+++ finapp/TunableTextField.swift
@@ -10,20 +10,36 @@ import UIKit
 
 class TunableTextField: UITextField {
 
-    enum TextType {
-        case NoValue
-        case Text
-        case IntegerNumber
-        case FloatNumber
+    enum TextType: String {
+        case NoValue = "NoValue"
+        case Text = "Text"
+        case IntegerNumber = "IntegerNumber"
+        case FloatNumber = "FloatNumber"
     }
     
-    enum CanBeEmpty {
-        case NoValue
-        case Can
-        case Cant
+    enum CanBeEmpty: String {
+        case NoValue = "NoValue"
+        case Can = "Can"
+        case Cant = "Cant"
     }
-    
-    var textType = TextType.NoValue
-    var canBeEmpty = CanBeEmpty.NoValue
+
+    @IBInspectable
+    var valiadtorID: String = ""
+    @IBInspectable
+    var textTypeAdapter: String {
+        get { return textType.rawValue }
+        // It would be beter if it crashes when there is a wrong string in storyboard
+        set { textType = TextType(rawValue: newValue)! }
+    }
+    var textType: TextType = TextType.NoValue
+    @IBInspectable
+    var CanBeEmptyAdapter: String {
+        get { return canBeEmpty.rawValue }
+        // It would be beter if it crashes when there is a wrong string in storyboard
+        set { canBeEmpty = CanBeEmpty(rawValue: newValue)! }
+    }
+    var canBeEmpty: CanBeEmpty = CanBeEmpty.NoValue
+    @IBInspectable
+    var regexpPattern: String = "."
 
 }
