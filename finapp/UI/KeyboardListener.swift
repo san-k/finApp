d3779f0e58ef429e883c74b7e3f82854cc815a5d
Admin <admin@mac-admin.local>
2017-02-07 00:24:28 +0200

    work around keyboard notifications 2

--- finapp/UI/KeyboardListener.swift
+++ finapp/UI/KeyboardListener.swift
@@ -7,10 +7,36 @@
 //
 
 import Foundation
+import UIKit
+
+protocol KeyboardListener : NotificationObserver{
+    
+//    func keyboardDidShow(withRect rect: CGRect)
+//    func keyboardWillhow(withRect rect: CGRect)
+//    func keyboardDidShow(withRect rect: CGRect)
+//    func keyboardDidShow(withRect rect: CGRect)
+}
+
+extension KeyboardListener {
+
+    
+    
+}
 
 
-protocol KeyboardListener {
+protocol NotificationObserver {}
+
+extension NotificationObserver where Self: AnyObject {
     
+    func observe(notification: Notification.Name, selector: Selector, fromPoster poster: Any?) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: poster)
+    }
     
+    func stopObserve(notification: Notification.Name, fromPoster poster: Any?) {
+        NotificationCenter.default.removeObserver(self, name: notification, object: poster)
+    }
     
+    func stopObserveAll() {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
--- finapp/UI/NewAccountViewController.swift
+++ finapp/UI/NewAccountViewController.swift
@@ -8,79 +8,8 @@
 
 import UIKit
 
-protocol NotificationObserver {
 
-    func foo(name: (Notification.Name)?)
-}
-
-extension NotificationObserver where Self: AnyObject {
-    
-    func observe<StringEnum: RawRepresentable>(forNotification notification: StringEnum) where StringEnum.RawValue == String {
-        let asd = notification
-        print("notification = \(notification)")
-        print("notification.rawValue = \(notification.rawValue)")
-//        foo(name: (notification.rawValue).map {
-//            NSNotification.Name(rawValue: $0)
-//        })
-        
-        NotificationCenter.default.addObserver(<#T##observer: Any##Any#>, selector: <#T##Selector#>, name: <#T##NSNotification.Name?#>, object: <#T##Any?#>)
-    }
-    
-}
-
-enum MyEnum {
-    case First(String)
-    case Second
-}
-
-
-//-(void)keyboardDidShow:(NSNotification*)notification
-//{
-//    if(self.isDescriptionEditing == NO)
-//    return;
-//    
-//    NSDictionary *dic = notification.userInfo;
-//    NSValue *keyboardFrame = dic[UIKeyboardFrameEndUserInfoKey];
-//    CGRect frame = [keyboardFrame CGRectValue];
-//    CGRect viewFrame = [self.view convertRect:frame fromView:nil];
-//    CGFloat keyboardHeight = viewFrame.size.height;
-//    
-//    
-//    
-//    UITableViewCell *descCell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:2]];
-//    CGFloat cellY = descCell.frame.origin.y;
-//    
-//    CGFloat descriptionBottom = cellY + self.descriptionTextView.frame.origin.y + self.descriptionTextView.bounds.size.height;
-//    CGFloat keyboardTop = self.tableView.bounds.size.height - keyboardHeight;
-//    CGFloat scrollOffset = descriptionBottom - keyboardTop;
-//    if(scrollOffset >0)
-//    {
-//        [UIView animateWithDuration:0.4 animations:^{
-//            self.tableView.contentOffset = CGPointMake(0, scrollOffset);
-//            }];
-//    }
-//    
-//}
-//
-//
-//public func keyboardWillAppear(frame keyboardRect: CGRect, animationDuration: Double) -> Void {
-//    guard let activeField = activeTextField else { return }
-//    guard let fieldContainerView = activeField.superview else { return }
-//    
-//    let activeFrame = fieldContainerView.frame
-//    let globalPoint: CGPoint = activeField.convert(CGPoint.zero, to: nil)
-//    let verticalOverlaping = globalPoint.y + fieldContainerView.bounds.height + kOffsetAboveKeyboard - (UIScreen.main.bounds.height - keyboardRect.height)
-//    
-//    if verticalOverlaping > 0 {
-//        UIView.animate(withDuration: animationDuration, animations: {
-//            var frame: CGRect = self.view.frame
-//            frame.origin.y -= verticalOverlaping
-//            self.view.frame = frame
-//        })
-//    }
-
-
-class NewAccountViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, NotificationObserver {
+class NewAccountViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
     @IBOutlet var nameText: UITextField!
     @IBOutlet var startSumText: UITextField!
@@ -90,20 +19,7 @@ class NewAccountViewController: UIViewController, UITableViewDelegate, UITableVi
     @IBOutlet weak var currencyLabel: UILabel!
     private var selectedCurrency: String?
     
-    
-    func foo(name: (Notification.Name)?) {
-        let asd = name
-        print("Notification.Name = \(asd)")
-        
-        let goodAsd: MyEnum = .First("asdasd")
-        let badAsd: MyEnum = .Second
-        
-        let asd3: Optional<Int> = 4
-        
-        
-    }
-    
-    
+
     // PRIVATE PROPERTIES
     private var createdAccount: FinAccount?
     private let currencyArr: [String] = {
@@ -115,7 +31,6 @@ class NewAccountViewController: UIViewController, UITableViewDelegate, UITableVi
     override func viewDidLoad() {
         setupTable()
         addBarButtons()
-        observe(forNotification: Notification.Name.NSCalendarDayChanged)
     }
     
     private func setupTable() {
