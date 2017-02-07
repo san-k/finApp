74e0994fae4563c4c64b89a092fe74aeccf6fadd
Oleksandr Kachanov <okacha@softserveinc.com>
2017-02-07 18:24:42 +0200

    Keyboard Listener implementation in progress

--- finapp/UI/KeyboardListener.swift
+++ finapp/UI/KeyboardListener.swift
@@ -9,27 +9,52 @@
 import Foundation
 import UIKit
 
-protocol KeyboardListener : NotificationObserver{
-    
-//    func keyboardDidShow(withRect rect: CGRect)
-//    func keyboardWillhow(withRect rect: CGRect)
-//    func keyboardDidShow(withRect rect: CGRect)
-//    func keyboardDidShow(withRect rect: CGRect)
+@objc protocol KeyboardListener: NotificationObserver {
+    func keyboardWillhow(withRect rect: CGRect)
+    //    func keyboardDidShow(withRect rect: CGRect)
+    //    func keyboardDidShow(withRect rect: CGRect)
+    //    func keyboardDidShow(withRect rect: CGRect)
 }
 
-extension KeyboardListener {
+extension KeyboardListener where Self: UIViewController {
+    
+    func observeKeyboardWillNotifications() {
 
+        observeFromAll(notification: Notification.Name.UIKeyboardWillShow) { [weakSelf = self] (notification) in
+            let keyboardRect = CGRect()
+            weakSelf.keyboardWillhow(withRect: keyboardRect)
+        }
+    }
+ 
+    
     
     
 }
 
+@objc protocol NotificationObserver {}
 
-protocol NotificationObserver {}
+extension NotificationObserver where Self : UIViewController {
+    
+    typealias NotificationClosure = (Notification) -> Void
+    
+    func observeFromAll(notification: Notification.Name, selector: Selector) {
+        observe(notification: notification, selector: selector, fromPoster: nil)
+    }
 
-extension NotificationObserver where Self: AnyObject {
+    func observeFromAll(notification: Notification.Name, using closure: @escaping NotificationClosure) {
+        observe(notification: notification, fromPoster: nil, using: closure)
+    }
     
     func observe(notification: Notification.Name, selector: Selector, fromPoster poster: Any?) {
         NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: poster)
+    }
+
+    func observe(notification: Notification.Name, fromPoster poster: Any?, using closure: @escaping NotificationClosure) {
+        NotificationCenter.default.addObserver(forName: notification, object: poster, queue: nil, using: closure)
+    }
+    
+    func stopObserveFromAll(notification: Notification.Name) {
+        stopObserve(notification: notification, fromPoster: nil)
     }
     
     func stopObserve(notification: Notification.Name, fromPoster poster: Any?) {
