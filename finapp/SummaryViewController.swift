fd8a33e17c8572787da17e5b3b71c0fff2d1fd00
Admin <admin@mac-admin.local>
2017-03-20 02:07:36 +0200

    started with summary controller

--- finapp/SummaryViewController.swift
+++ finapp/SummaryViewController.swift
@@ -16,12 +16,38 @@ class SummaryViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let defaultAcoount = AppSettings.sharedSettings.defaultAccount {
+            defaultAcoountButton.setTitle("Default account is \(defaultAcoount.name)", for: UIControlState.normal)
+        } else {
+            defaultAcoountButton.setTitle("Select default account", for: UIControlState.normal)
+        }
+    }
 
+    @IBOutlet weak var previousMounthPut: UILabel!
+    @IBOutlet weak var previousMountsTake: UILabel!
+    @IBOutlet weak var currentMounthPut: UILabel!
+    @IBOutlet weak var currentMountsTake: UILabel!
+    @IBOutlet weak var currentWeekPut: UILabel!
+    @IBOutlet weak var currentWeekTake: UILabel!
+    
+    fileprivate let dataSource = AppSettings.sharedSettings.datasource
+    
+    @IBOutlet weak var defaultAcoountButton: UIButton!
 
+    @IBAction func selectDefaultAccount(_ sender: Any) {
+        showAccountsController(with: AccountsViewController.AccountsControllerMode.selectDefaultAccount)
+    }
+    
     @IBAction func show(_ sender: Any) {
-     
+        showAccountsController(with: AccountsViewController.AccountsControllerMode.showTransactions)
+    }
+    
+    fileprivate func showAccountsController(with mode: AccountsViewController.AccountsControllerMode) {
         let controller = AccountsViewController()
-        controller.controllerMode = AccountsViewController.AccountsControllerMode.showTransactions
+        controller.controllerMode = mode
         let navcontroller = self.navigationController
         navcontroller?.pushViewController(controller, animated: true)
     }
@@ -29,10 +55,13 @@ class SummaryViewController: UIViewController {
    
     @IBAction func showCAtegories(_ sender: UIButton) {
 
+        if AppSettings.sharedSettings.defaultAccount == nil {
+            return
+        }
+
         let storyboard = UIStoryboard(name: "Categories", bundle: nil)
         guard let controller = storyboard.instantiateInitialViewController() as? CategoriesViewController else {return}
-        // hardcoded part, should be changed to take info from NSUserDefaults
-        controller.account = AppSettings.sharedSettings.datasource.getAllFinAccounts()?.first
+        controller.account = AppSettings.sharedSettings.defaultAccount
         let nav = UINavigationController(rootViewController: controller)
         nav.navigationBar.isTranslucent = false
         navigationController?.present(nav, animated: true, completion: nil)
