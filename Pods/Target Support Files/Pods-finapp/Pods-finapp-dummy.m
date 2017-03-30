00414e4be1461bda1ed60c6ad006a582613e3fde
Oleksandr Kachanov <okacha@softserveinc.com>
2017-03-30 19:58:56 +0300

    added chameleon pod

---
+++ Pods/Target Support Files/Pods-finapp/Pods-finapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_finapp : NSObject
+@end
+@implementation PodsDummy_Pods_finapp
+@end
---
+++ finapp/myView.swift
@@ -0,0 +1,16 @@
+//
+//  myView.swift
+//  finapp
+//
+//  Created by Oleksandr Kachanov on 3/20/17.
+//  Copyright © 2017 Oleksandr Kachanov. All rights reserved.
+//
+
+import UIKit
+
+class myView: UIView {
+
+    
+    
+
+}
